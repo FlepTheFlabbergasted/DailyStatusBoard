b5e0a297eb7342c26ba0efb309e03af858224254
Strandberg Filip <strandbergfilip94@gmail.com>
2018-11-30 15:54:33 +0100

    Added folders css and scripts, added files for post-it notes

---
+++ scripts/dragndrop.js
@@ -0,0 +1,30 @@
+
+// Code from: https://stackoverflow.com/questions/6230834/html5-drag-and-drop-anywhere-on-the-screen
+
+function drag_start(event) {
+	var style = window.getComputedStyle(event.target, null);
+	var str = (parseInt(style.getPropertyValue("left")) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top")) - event.clientY)+ ',' + event.target.id;
+	event.dataTransfer.setData("Text",str);
+} 
+
+function drop(event) {
+	var offset = event.dataTransfer.getData("Text").split(',');
+	var dm = document.getElementById(offset[2]);
+	dm.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
+	dm.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
+	event.preventDefault();
+	return false;
+}
+
+function drag_over(event) {
+	event.preventDefault();
+	return false;
+}  
+
+// TODO: Add eventlisteners to all stuff
+/*
+var dm = document.getElementById('dragme'); 
+dm.addEventListener('dragstart',drag_start,false); 
+document.body.addEventListener('dragover',drag_over,false); 
+document.body.addEventListener('drop',drop,false); 
+*/
