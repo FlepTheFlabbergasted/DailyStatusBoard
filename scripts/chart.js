bfc11d3f4fa04b74c680ac1a9b5ec1465ba02856
Simon Andersson <simon.a.andersson@tieto.com>
2019-02-25 14:30:16 +0100

    Add dynamic postits

--- scripts/chart.js
+++ scripts/chart.js
@@ -37,6 +37,10 @@ const Y_AXES_TICKS_SUGGESTED_MAX = 5;
 var secretCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 var secretCodeIndex = 0;
 
+var nrOfPostits = 0;
+const MAX_NR_OF_POSTITS = 8;
+var POSTITS = [];
+
 // Dates for each data entry
 var DATA_DATES = [];
 // Comment for each data entry
@@ -288,6 +292,13 @@ function setPostitTextAreas(postitId, defaultText){
 	}
 }
 
+function addPostit(postit){
+	$('<div class="postit" draggable="true" ondragstart="drag_start(event)" id=' + 
+	postit[0] + 
+	'><textarea id=' + postit[1] +
+	'>' + postit[2] + '</textarea>').appendTo(document.getElementById('postitContainer'));
+}
+
 window.onload = function() {
 	// Create new ChartHandler to control the chart, all further operations will be 
 	// called using 'window.chartHandler'
@@ -298,26 +309,37 @@ window.onload = function() {
 		Cookies.set('AllTimeStatsData', [0, 0]);
 		window.statsHandler = new StatsHandler(0, 0);
 	}
+	
+	let postits = Cookies.getJSON('postits');
+	if(postits != undefined){
+		nrOfPostits = postits.length;
+		for (let i = 0; i < nrOfPostits; i++) {
+			POSTITS[i] = postits[i];
+			addPostit(postits[i]);
+			//Do something
+		}
+	}
 	window.chartHandler = new ChartHandler();
-	setPostitTextAreas("filipText", "Filip");
+	/*setPostitTextAreas("filipText", "Filip");
 	setPostitTextAreas("jorgenText", "Jörgen");
 	setPostitTextAreas("jakobText", "Jakob");
 	setPostitTextAreas("mattiasText", "Mattias");
 	setPostitTextAreas("mickeText", "Mikael");
 	setPostitTextAreas("simonText", "Simon");
 	setPostitTextAreas("figaroText", "Figaro");
-	setPostitTextAreas("niklasText", "Niklas");
+	setPostitTextAreas("niklasText", "Niklas");*/
 }; // window.onload
 
 window.onbeforeunload = function(){
-   Cookies.set('filipText', document.getElementById("filipText").value);
+	Cookies.set('postits', POSTITS);
+   /*Cookies.set('filipText', document.getElementById("filipText").value);
    Cookies.set('jorgenText', document.getElementById("jorgenText").value);
    Cookies.set('jakobText', document.getElementById("jakobText").value);
    Cookies.set('mattiasText', document.getElementById("mattiasText").value);
    Cookies.set('mickeText', document.getElementById("mickeText").value);
    Cookies.set('simonText', document.getElementById("simonText").value);
    Cookies.set('figaroText', document.getElementById("figaroText").value);
-   Cookies.set('niklasText', document.getElementById("niklasText").value);
+   Cookies.set('niklasText', document.getElementById("niklasText").value);*/
 }
 
 $(document).ready(function() {
@@ -407,6 +429,20 @@ $(document).ready(function() {
 			// Do nothing!
 		}
 	});
+	
+	document.getElementById('addPostit').addEventListener('click', function() {
+		if(nrOfPostits < MAX_NR_OF_POSTITS){
+			DEBUG_LOG('Adding postit...');
+			let initialText = prompt("Text", "Add Your Text Here");
+			let postit = ["postit" + nrOfPostits, "text" + nrOfPostits, initialText];
+			POSTITS[nrOfPostits] = postit;
+			addPostit("postit" + nrOfPostits, "text" + nrOfPostits, initialText);
+			nrOfPostits++;
+		}else{
+			DEBUG_LOG('Max number of postits already added (' + MAX_NR_OF_POSTITS + ')');
+			alert("Max number of postits already added...");
+		}
+	});
 
 	document.getElementById('submitData').addEventListener('click', function() {
 		submitData();
@@ -436,14 +472,14 @@ $(document).ready(function() {
 	document.getElementById('plusInput').readOnly = true;
 	document.getElementById('minusInput').readOnly = true;
 	document.getElementById("statsCanvas").style.display = "none";
-	document.getElementById('filipText').value = 'Filip:\n';
+	/*document.getElementById('filipText').value = 'Filip:\n';
 	document.getElementById('jorgenText').value = 'Jörgen:\n';
 	document.getElementById('jakobText').value = 'Jakob:\n';
 	document.getElementById('mattiasText').value = 'Mattias:\n';
 	document.getElementById('mickeText').value = 'Mikael:\n';
 	document.getElementById('simonText').value = 'Simon:\n';
 	document.getElementById('figaroText').value = 'Figaro:\n';
-	document.getElementById('niklasText').value = 'Niklas:\n';
+	document.getElementById('niklasText').value = 'Niklas:\n';*/
 
 	function toggleView(){
 		// Update the statsHandler today's data
@@ -568,6 +604,13 @@ $(document).ready(function() {
 		}else{
 			secretCodeIndex = 0;
 		}
+	}
+	
+	function addPostit(postitId, textAreaId, initalText){
+		$('<div class="postit" draggable="true" ondragstart="drag_start(event)" id=' + 
+		postitId + 
+		'><textarea id=' + textAreaId +
+		'>' + initalText + '</textarea>').appendTo(document.getElementById('postitContainer'));
 	}
 
 	document.addEventListener('keyup', doc_keyUp, false);
