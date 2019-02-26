e29d37a0359109be21885aaca4fb7421a318fc18
Simon Andersson <simon.a.andersson@tieto.com>
2019-02-26 15:58:09 +0100

    Cleanup add postits and remove save position

--- scripts/chart.js
+++ scripts/chart.js
@@ -37,9 +37,14 @@ const Y_AXES_TICKS_SUGGESTED_MAX = 5;
 var secretCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 var secretCodeIndex = 0;
 
-var nrOfPostits = 0;
+var NR_OF_POSTITS = 0;
 const MAX_NR_OF_POSTITS = 8;
-var POSTITS = [];
+var AVAIL_POSTIT_INDEXES = [];
+const POSTIT_ID = 0;
+const POSTIT_TEXT_ID = 1;
+const POSTIT_TEXT = 2;
+const POSTIT_POS_LEFT = 3;
+const POSTIT_POS_TOP = 4;
 
 // Dates for each data entry
 var DATA_DATES = [];
@@ -283,44 +288,41 @@ class ChartHandler {
 	}
 } // class ChartHandler
 
-function setPostitTextAreas(postitId, defaultText){
-	let cookie = Cookies.get(postitId);
-	if(cookie != undefined){
-		document.getElementById(postitId).value = cookie;
-	}else{
-		document.getElementById(postitId).value = defaultText;
-	}
-}
-
-function addPostit(postitId, textAreaId, initalText, posLeft, posTop){
-	let postitDiv = $('<div class="postit" draggable="true" ondragstart="drag_start(event)" id=' + 
-	postitId + 
+function getPostitHTML(postitId, textAreaId, initialText){
+	let postitDiv = $('<div class="postit" draggable="true" ondragstart="drag_start(event)" id=' +
+	postitId +
 	'><textarea id=' + textAreaId +
-	'>' + initalText + '</textarea></div>');
+	'>' + initialText + '</textarea></div>');
 	let deleteButton = $('<button id="removePostit">X</button>');
 	deleteButton.appendTo(postitDiv);
-	postitDiv.appendTo(document.getElementById('postitContainer'));
-	postitDiv.appendTo(document.getElementById('postitContainer'));
-	if(parseInt(posLeft) !== -1 || parseInt(posTop) !== -1){
-		var d = document.getElementById(postitId);
-		d.style.position = "absolute";
-		d.style.left = posLeft+'px';
-		d.style.top = posTop+'px';
-	}
+	return postitDiv;
 }
 
-function findFreePostitIndex(){
-    for (var i = 0; i < POSTITS.length; i++) {
-        if(POSTITS[i] == false){
+function addPostit(postitId, textAreaId, initialText){
+	$('#postitContainer').append(getPostitHTML(postitId, textAreaId, initialText));
+}
+
+function addPostitWithPosition(postitId, textAreaId, initialText, posLeft, posTop){
+	$('#postitContainer').append(getPostitHTML(postitId, textAreaId, initialText));
+
+	var d = document.getElementById(postitId);
+	d.style.position = "absolute";
+	d.style.left = posLeft+'px';
+	d.style.top = posTop+'px';
+}
+
+function allocatePostitIndex(){
+    for (var i = 0; i < AVAIL_POSTIT_INDEXES.length; i++) {
+        if(AVAIL_POSTIT_INDEXES[i] == false){
 			return i;
 		}
     }
-	return false;
+    return false;
 }
 
 function resetPostitIndexAllocation(){
 	for(var i = 0; i < MAX_NR_OF_POSTITS; i++){
-		POSTITS[i] = false;
+		AVAIL_POSTIT_INDEXES[i] = false;
 	}
 }
 
@@ -334,16 +336,18 @@ window.onload = function() {
 		Cookies.set('AllTimeStatsData', [0, 0]);
 		window.statsHandler = new StatsHandler(0, 0);
 	}
-	
+
 	resetPostitIndexAllocation();
-	
+
 	let postits = Cookies.getJSON('postits');
 	if(postits != undefined){
-		nrOfPostits = postits.length;
-		for (let i = 0; i < nrOfPostits; i++) {
-			postitIndex = postits[i][0][postits[i][0].length -1];
-			POSTITS[postitIndex] = true;
-			addPostit(postits[i][0], postits[i][1], postits[i][2], postits[i][3], postits[i][4]);
+		NR_OF_POSTITS = postits.length;
+		for (let i = 0; i < NR_OF_POSTITS; i++) {
+			postitIndex = postits[i][POSTIT_ID][postits[i][POSTIT_ID].length -1];
+			AVAIL_POSTIT_INDEXES[postitIndex] = true;
+			addPostit(postits[i][POSTIT_ID],
+									postits[i][POSTIT_TEXT_ID],
+									postits[i][POSTIT_TEXT]);
 		}
 	}
 	window.chartHandler = new ChartHandler();
@@ -446,15 +450,15 @@ $(document).ready(function() {
 			// Do nothing!
 		}
 	});
-	
+
 	document.getElementById('addPostit').addEventListener('click', function() {
-		if(nrOfPostits < MAX_NR_OF_POSTITS){
+		if(NR_OF_POSTITS < MAX_NR_OF_POSTITS){
 			DEBUG_LOG('Adding postit...');
 			let initialText = prompt("Text", "Add Your Text Here");
-			postitIndex = findFreePostitIndex();
-			POSTITS[postitIndex] = true;
-			addPostit("postit" + postitIndex, "text" + postitIndex, initialText, -1, -1);
-			nrOfPostits++;
+			postitIndex = allocatePostitIndex();
+			AVAIL_POSTIT_INDEXES[postitIndex] = true;
+			addPostit("postit" + postitIndex, "text" + postitIndex, initialText);
+			NR_OF_POSTITS++;
 		}else{
 			DEBUG_LOG('Max number of postits already added (' + MAX_NR_OF_POSTITS + ')');
 			alert("Max number of postits already added...");
@@ -614,12 +618,12 @@ $(document).ready(function() {
 			secretCodeIndex = 0;
 		}
 	}
-	
+
 	$(document).on('click','#removePostit',function(){
 		let postit = this.parentNode;
 		postitIndex = postit.id[postit.id.length -1];
-		POSTITS[postitIndex] = false;
-		nrOfPostits--;
+		AVAIL_POSTIT_INDEXES[postitIndex] = false;
+		NR_OF_POSTITS--;
 		this.parentNode.parentNode.removeChild(postit);
 	});
 
